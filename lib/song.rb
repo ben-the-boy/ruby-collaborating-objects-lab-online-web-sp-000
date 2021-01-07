5063336954440b5e81570cff9bf928582b11022b
Ben Hayes <b.t.hayes@comcast.net>
2021-01-07 19:35:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,7 @@ class Song
   
   def self.new_by_filename(filename)
     song = Song.new(filename.split(" - ")[1])
+    song.artist = filename.split(" - ")[0]
   end 
   
   def artist_name=(name)
